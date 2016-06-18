e387ea88c852d73c848d8c3a76ea56026a1a5d12
Mauro A Flores A <mauroa_99@hotmail.com>
2016-06-18 14:24:35 -0500

    Initial Commit

---
+++ Hamburguesas en el mundo/Datos.swift
@@ -0,0 +1,33 @@
+//
+//  Datos.swift
+//  Hamburguesas en el mundo
+//
+//  Created by Mauro Alberto Flores Almonte on 18/06/16.
+//  Copyright © 2016 Mauro Alberto Flores Almonte. All rights reserved.
+//
+
+import Foundation
+
+class ColeccionDePaises {
+    let paises = [ "Canadá", "Estados Unidos De América", "Finlandia", "México", "San Pedro Y Miquelón" , "Groenlandia", "Otros Países Territorios De América Del Norte", "Antigua Y Barbuda", "Bahamas", "Barbados", "Costa Rica", "Cuba", "Dominica", "El Salvador", "Granada", "Guatemala", "Haití", "Honduras", "Jamaica", "Nicaragua", "Panamá", "San Vicente Y Las Granadinas", "Republica Dominicana", "Trinidad Y Tobago", "Santa Lucia", "San Cristóbal Y Nieves", "Islas Caimán", "Islas Turcas Y Caicos", "Islas Vírgenes De Los Estados Unidos", "Guadalupe", "Antillas Holandesas", "San Martin", "Aruba", "Montserrat", "Anguilla", "Martinica", "Puerto Rico", "Bermudas", "Islas Vírgenes Británicas", "Otros Países Territorios Del Caribe Y América Central", "Argentina", "Bolivia", "Brasil", "Colombia", "Chile", "Ecuador", "Guyana", "Paraguay", "Perú", "Surinam", "Uruguay", "Venezuela", "Guayana Francesa", "Islas Malvinas", "Otros Países ó Territorios  De Sudamérica"]
+    
+    func obtenPais() -> String {
+        let pais = Int (arc4random()) % paises.count
+        return paises[pais]
+        
+        }
+}
+
+    
+class ColeccionDeHamburguesas {
+        let menuDeHamburguesas = [ "Hamburguesa de Pimientos", "Hamburguesa de Cordero", "Hamburguesa de Frijoles", "Hamburguesa de Pollo", "Hamburguesa Vegetariana", "Hamburguesa Mediterránea", "Hamburguesa Ibérica", "Hamburguesa Tradicional", "Hamburguesa de Cangrejo", "Hamburguesa de Camarones", "Hamburguesa de Atún", "Hamburguesa de Soya", "Hamburguesa La Golf", "Hamburguesa Mexicana", "Hamburguesa Cheese", "Hamburguesa Parrillera", "Hamburguesa a la Pobre", "Hamburguesa Francesa", "Hamburguesa Hawaiana", "Hamburguesa Doble Queso", "Hamburguesa a la Swift" ]
+        
+        func obtenHamburguesa() -> String {
+            let hamburgesa = Int (arc4random()) % menuDeHamburguesas.count
+            return menuDeHamburguesas[hamburgesa]
+        }
+    
+    }
+
+
+
---
+++ Hamburguesas en el mundo/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Hamburguesas en el mundo
+//
+//  Created by Mauro Alberto Flores Almonte on 18/06/16.
+//  Copyright © 2016 Mauro Alberto Flores Almonte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var pais: UILabel!
+    
+    @IBOutlet weak var hamburguesa: UILabel!
+    
+    let paises = ColeccionDePaises()
+    
+    let hamburguesas = ColeccionDeHamburguesas ()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    @IBAction func quieroUnaHamburguesa() {
+        pais.text = paises.obtenPais()
+        hamburguesa.text = hamburguesas.obtenHamburguesa()
+        
+        
+        
+    }
+    
+
+}
+
